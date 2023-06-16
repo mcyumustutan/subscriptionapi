396cab35d9b1b9999d5eb0e2110759372a3eab76
mcyumustutan <mehmetcy01@gmail.com>
2023-06-16 11:03:10 +0300

    Os cast edited

--- app/Models/Devices.php
+++ app/Models/Devices.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Enums\Os;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Devices extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'os' => Os::class
+    ];
 }
