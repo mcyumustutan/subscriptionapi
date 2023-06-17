708c7da57773f23b259f03f490bff1af7a7f6186
mcyumustutan <mehmetcy01@gmail.com>
2023-06-17 23:48:58 +0300

    Worker operations added

---
+++ app/Console/Commands/ExpiredSubscriptions.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\ValidationAction;
+use App\Interfaces\SubscriptionInterface;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
+
+class ExpiredSubscriptions extends Command
+{
+
+    public function __construct(
+        private readonly SubscriptionInterface $subscriptionInterface,
+        private readonly ValidationAction      $validationAction,
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:expired-subscriptions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Find expired non canceled subscriptions';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $subscriptions = $this->subscriptionInterface->getExpiredsNonCanceled()
+            ->chunk(200, function (Collection $subscriptions) {
+                foreach ($subscriptions as $subscription) {
+
+                    $validationResult = $this->validationAction->execute($subscription->receipt);
+                    if (!$validationResult['status']) {
+                        return response()->json($validationResult, 200);
+                    }
+
+                    $this->subscriptionInterface->subscription(
+                        $validationResult['expire-date'],
+                        $subscription->device_id,
+                        $subscription->receipt
+                    );
+                }
+            });
+
+    }
+}
